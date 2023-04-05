fa23f2152d3901b9c0fab580d1376afc11470eae
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-05 09:32:15 +0300

    loadMore

--- src/js/img_search/img_search.js
+++ src/js/img_search/img_search.js
@@ -1,4 +1,5 @@
 import { Notify } from "notiflix";
+import Notiflix from 'notiflix';
 import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css"
 import { headerOfClassSearch } from '../mainMarkup'
@@ -17,20 +18,56 @@ document.querySelector('#root').innerHTML = headerOfClassSearch
 export const refs = {
     head: document.querySelector('.header'),
     form: document.querySelector('#search-form'),
+    input: document.querySelector('.field'),
     sub: document.querySelector('.search-btn'),
-    more: document.querySelector('.load-more'),
+    loadMore: document.querySelector('.load-more'),
     gal : document.querySelector('.gallery'),
     toTop : document.querySelector('.back-to-top'),
 }
 
-refs.form.addEventListener('click', onSearch)
-refs.more.addEventListener('click', onLoadMore)
+refs.form.addEventListener('submit', onSearch)
+refs.loadMore.addEventListener('click', onLoadMore)
+refs.input.addEventListener('input', onInputChange)
 
-function onSearch() {
+let query = refs.input.value
+const page = 1 
+const perPage = 40
 
+function onSearch(e) {
+    e.preventDefault()
+    query = refs.input.value.trim()
+    refs.sub.disabled = true
+    refs.gal.innerHTML = '';
+    refs.loadMore.classList.add('is-hidden');
+
+
+    if (query === '') {
+        Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
+        return;
+      }
+    if ( query.length === 1) {
+        Notiflix.Notify.failure('The  query is too short. Please specify your search query.');
+        return;
+    }
+
+    console.log(refs.input.value);
+    fetchCard(query,page,perPage)
+    .then(( data) => {
+        const {hits, totalHits, total } = data
+        console.log('hits', hits, 'totalHits', totalHits, 'total',total) ;
+
+        if (totalHits === 0) {
+            Notiflix.Notify.failure(
+                'Sorry, there are no images matching your search query. Please try again.',
+              )}
+    })
 }
 
-function onLoadMore() {
+function onLoadMore(e) {
+    e.preventDefault()
+}
 
+function onInputChange() {
+    refs.sub.disabled = false
 }
 
