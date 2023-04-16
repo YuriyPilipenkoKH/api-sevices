096079f14a8a9139f9b78a0b3548ae6d48b9c227
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-16 07:54:59 +0300

    go

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 import { foxNick,lens,testsvg, iconUser, iconUser2, iconUser3 } from "./js/mainMarkup";
 
-//  document.querySelector('body').insertAdjacentHTML('beforeend', iconUser)
+ document.querySelector('body').insertAdjacentHTML('beforeend', iconUser)
 
-//testsvg
+testsvg
 
 
